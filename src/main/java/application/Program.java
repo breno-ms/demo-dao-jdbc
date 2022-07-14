ff5d3cc13a2526501379e89c723d27953e4bab4e
breno-ms <breno.wrk@gmail.com>
2022-07-14 08:33:09 -0300

    Criando a entidade Seller

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -1,12 +1,17 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
 
     public static void main(String[] args) {
 
-        
+        Department department = new Department(1, "Administrativo");
+        Seller seller = new Seller(1, "Bob", "bob@email.com", new Date(), 3000.0, department);
+        System.out.println(seller);
 
     }
 
